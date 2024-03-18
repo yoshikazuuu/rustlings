25f4f995098edff51142e8985ce6650dc8414587
yoshikazuuu <febrianojerry@gmail.com>
2024-03-18 23:25:18 +0700

    session 6

--- exercises/traits/traits3.rs
+++ exercises/traits/traits3.rs
@@ -8,10 +8,10 @@
 // Execute `rustlings hint traits3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub trait Licensed {
-    fn licensing_info(&self) -> String;
+    fn licensing_info(&self) -> String {
+        String::from("")
+    }
 }
 
 struct SomeSoftware {
@@ -22,8 +22,17 @@ struct OtherSoftware {
     version_number: String,
 }
 
-impl Licensed for SomeSoftware {} // Don't edit this line
-impl Licensed for OtherSoftware {} // Don't edit this line
+impl Licensed for SomeSoftware {
+    fn licensing_info(&self) -> String {
+        String::from("Some information")
+    }
+}
+
+impl Licensed for OtherSoftware {
+    fn licensing_info(&self) -> String {
+        String::from("Some information")
+    }
+}
 
 #[cfg(test)]
 mod tests {
